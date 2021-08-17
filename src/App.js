e8b30c38e66a0b3f8fdb8ae22d7e843dae1a0ba2
Caleb Anderson <buyananderson@gmail.com>
2021-08-17 15:54:59 +0200

    Put class based component in App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React, { Component } from 'react';
 import Calculator from './components/Calculator';
 import './Sass/app.css';
 
-function App() {
-  return <Calculator />;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
