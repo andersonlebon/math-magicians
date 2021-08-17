e901f4fe4f944666c40f4bb2350f56fddcbc1378
Caleb Anderson <buyananderson@gmail.com>
2021-08-17 14:22:35 +0200

    Add oparetion icons

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  TiDivide, TiTimes, TiMinus, TiPlus, TiEquals,
+} from 'react-icons/ti';
 
 const Calculator = () => (
   <div className="container">
@@ -7,26 +10,36 @@ const Calculator = () => (
       <li className="cal-item">AC</li>
       <li className="cal-item">+/-</li>
       <li className="cal-item">%</li>
+      <li className="cal-item bg-orange">
+        <TiDivide />
+      </li>
 
       <li className="cal-item">7</li>
       <li className="cal-item">8</li>
       <li className="cal-item">9</li>
-      <li className="cal-item"> X</li>
+      <li className="cal-item bg-orange">
+        <TiTimes />
+      </li>
 
       <li className="cal-item">4</li>
       <li className="cal-item">5</li>
       <li className="cal-item">6</li>
-      <li className="cal-item">-</li>
+      <li className="cal-item bg-orange">
+        <TiMinus />
+      </li>
 
       <li className="cal-item">1</li>
       <li className="cal-item">2</li>
       <li className="cal-item">3</li>
-      <li className="cal-item">+</li>
+      <li className="cal-item bg-orange">
+        <TiPlus />
+      </li>
 
-      <li className="cal-item">4</li>
       <li className="cal-item item-0">0</li>
       <li className="cal-item">.</li>
-      <li className="cal-item">=</li>
+      <li className="cal-item bg-orange">
+        <TiEquals />
+      </li>
     </ul>
   </div>
 );
