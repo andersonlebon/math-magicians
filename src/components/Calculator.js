e0010052dc59477575505b41d13d3e65ac304976
Caleb Anderson <buyananderson@gmail.com>
2021-08-17 22:33:31 +0200

    Implement the calculate function the calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,51 +1,96 @@
+/* eslint-disable */
+
 import React, { Component } from 'react';
-import {
-  TiDivide, TiTimes, TiMinus, TiPlus, TiEquals,
-} from 'react-icons/ti';
+import { TiDivide, TiTimes, TiMinus, TiPlus, TiEquals } from 'react-icons/ti';
+import calculate from './calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      obj: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
   }
 
+  handleCalculate = (e) => {
+    const { obj } = this.state;
+    this.setState({ obj: calculate(obj, e.target.outerText) });
+    console.log(this.state.obj)
+  };
+
   render() {
+    const { obj: object } = this.state;
     return (
       <div className="container">
-        <div className="result">0</div>
+        <div className="result">{object.total|| object.next}</div>
         <ul className="cal-table">
-          <li className="cal-item">AC</li>
-          <li className="cal-item">+/-</li>
-          <li className="cal-item">%</li>
-          <li className="cal-item bg-orange">
+          <li className="cal-item" name="caleb" onClick={this.handleCalculate}>
+            AC
+          </li>
+          <li
+            className="cal-item onClick={this.handleCalculate}"
+            onClick={this.handleCalculate}
+          >
+            +/-
+          </li>
+          <li className="cal-item" onClick={this.handleCalculate}>
+            %
+          </li>
+          <li className="cal-item bg-orange" onClick={this.handleCalculate}>
             <TiDivide />
           </li>
 
-          <li className="cal-item">7</li>
-          <li className="cal-item">8</li>
-          <li className="cal-item">9</li>
-          <li className="cal-item bg-orange">
+          <li className="cal-item" onClick={this.handleCalculate}>
+            7
+          </li>
+          <li className="cal-item" onClick={this.handleCalculate}>
+            8
+          </li>
+          <li className="cal-item" onClick={this.handleCalculate}>
+            9
+          </li>
+          <li className="cal-item bg-orange" onClick={this.handleCalculate}>
             <TiTimes />
           </li>
 
-          <li className="cal-item">4</li>
-          <li className="cal-item">5</li>
-          <li className="cal-item">6</li>
-          <li className="cal-item bg-orange">
-            <TiMinus />
+          <li className="cal-item" onClick={this.handleCalculate}>
+            4
+          </li>
+          <li className="cal-item" onClick={this.handleCalculate}>
+            5
+          </li>
+          <li className="cal-item" onClick={this.handleCalculate}>
+            6
+          </li>
+          <li className="cal-item bg-orange" onClick={this.handleCalculate}>
+            -
           </li>
 
-          <li className="cal-item">1</li>
-          <li className="cal-item">2</li>
-          <li className="cal-item">3</li>
-          <li className="cal-item bg-orange">
+          <li className="cal-item" onClick={this.handleCalculate}>
+            1
+          </li>
+          <li className="cal-item" onClick={this.handleCalculate}>
+            2
+          </li>
+          <li className="cal-item" onClick={this.handleCalculate}>
+            3
+          </li>
+          <li className="cal-item bg-orange " onClick={this.handleCalculate}>
             <TiPlus />
           </li>
 
-          <li className="cal-item item-0">0</li>
-          <li className="cal-item">.</li>
-          <li className="cal-item bg-orange">
-            <TiEquals />
+          <li className="cal-item item-0" onClick={this.handleCalculate}>
+            0
+          </li>
+          <li className="cal-item" onClick={this.handleCalculate}>
+            .
+          </li>
+          <li className="cal-item bg-orange" onClick={this.handleCalculate}>
+            =
           </li>
         </ul>
       </div>
