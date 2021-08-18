26873470a3404d7177caf25f4fe8d791d0acb855
Caleb Anderson <buyananderson@gmail.com>
2021-08-18 12:06:10 +0200

    Update the state using hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,29 +3,39 @@ import {
   TiDivide, TiTimes, TiMinus, TiPlus, TiEquals,
 }
   from 'react-icons/ti';
-// import calculate from '../mathLogic/calculate';
+import calculate from '../mathLogic/calculate';
 
 const Calculator = () => {
-  const [state] = useState({
+  const [state, setState] = useState({
     total: null,
     next: null,
     operation: null,
   });
 
-  const handleCalculate = (e) => {
-    console.log(e.target);
-    // try {
-    //   const btnName = btn.outerText === '' ? btn.id : btn.outerText;
-    //   const obj = calculate(state, btnName);
-    //   setState(obj);
-    // } catch (error) {
-    //   const btnName = btn.outerText === '' ? btn.id : btn.outerText;
-    //   const { next } = state;
-    //   if (next) {
-    //     setState({ total: next, next: null });
-    //   }
-    //   setState({ operation: btnName });
-    // }
+  const handleCalculate = ({ currentTarget: btn }) => {
+    console.log('hello');
+    try {
+      const btnName = btn.outerText === '' ? btn.id : btn.outerText;
+      const obj = calculate(state, btnName);
+      const { next, operation, total } = obj;
+      let checkTotal;
+      let checkOp;
+      const { total: stateTotal, operation: stateOp } = state;
+      if (!total && stateTotal) {
+        checkTotal = stateTotal;
+      } else checkTotal = total;
+      if (!operation && stateOp) {
+        checkOp = stateOp;
+      } else checkOp = operation;
+      setState({ next, operation: checkOp, total: checkTotal });
+    } catch (error) {
+      const btnName = btn.outerText === '' ? btn.id : btn.outerText;
+      const { next } = state;
+      if (next) {
+        setState({ total: next, next: null });
+      }
+      setState({ operation: btnName });
+    }
   };
 
   const { next, total } = state;
@@ -35,12 +45,12 @@ const Calculator = () => {
         {total && next ? next : total || next || '0'}
       </div>
       <ul className="cal-table">
-        <li className="cal-item" onClick={() => handleCalculate()} aria-hidden="true">
+        <li className="cal-item" onClick={(e) => handleCalculate(e)} aria-hidden="true">
           AC
         </li>
         <li
-          className="cal-item onClick={() => handleCalculate}"
-          onClick={() => handleCalculate}
+          className="cal-item"
+          onClick={(e) => handleCalculate(e)}
           aria-hidden="true"
         >
           +/-
@@ -48,7 +58,7 @@ const Calculator = () => {
         <li
           className="cal-item"
           id="%"
-          onClick={() => handleCalculate}
+          onClick={(e) => handleCalculate(e)}
           aria-hidden="true"
         >
           %
@@ -56,63 +66,63 @@ const Calculator = () => {
         <li
           className="cal-item bg-orange"
           id="÷"
-          onClick={() => handleCalculate}
+          onClick={(e) => handleCalculate(e)}
           aria-hidden="true"
         >
           <TiDivide />
         </li>
 
-        <li className="cal-item" onClick={() => handleCalculate} aria-hidden="true">
+        <li className="cal-item" onClick={(e) => handleCalculate(e)} aria-hidden="true">
           7
         </li>
-        <li className="cal-item" onClick={() => handleCalculate} aria-hidden="true">
+        <li className="cal-item" onClick={(e) => handleCalculate(e)} aria-hidden="true">
           8
         </li>
-        <li className="cal-item" onClick={() => handleCalculate} aria-hidden="true">
+        <li className="cal-item" onClick={(e) => handleCalculate(e)} aria-hidden="true">
           9
         </li>
         <li
           className="cal-item bg-orange"
           id="x"
-          onClick={() => handleCalculate}
+          onClick={(e) => handleCalculate(e)}
           aria-hidden="true"
         >
           <TiTimes />
         </li>
 
-        <li className="cal-item" onClick={() => handleCalculate} aria-hidden="true">
+        <li className="cal-item" onClick={(e) => handleCalculate(e)} aria-hidden="true">
           4
         </li>
-        <li className="cal-item" onClick={() => handleCalculate} aria-hidden="true">
+        <li className="cal-item" onClick={(e) => handleCalculate(e)} aria-hidden="true">
           5
         </li>
-        <li className="cal-item" onClick={() => handleCalculate} aria-hidden="true">
+        <li className="cal-item" onClick={(e) => handleCalculate(e)} aria-hidden="true">
           6
         </li>
 
         <li
           className="cal-item bg-orange"
           id="-"
-          onClick={() => handleCalculate}
+          onClick={(e) => handleCalculate(e)}
           aria-hidden="true"
         >
           <TiMinus />
         </li>
 
-        <li className="cal-item" onClick={() => handleCalculate} aria-hidden="true">
+        <li className="cal-item" onClick={(e) => handleCalculate(e)} aria-hidden="true">
           1
         </li>
-        <li className="cal-item" onClick={() => handleCalculate} aria-hidden="true">
+        <li className="cal-item" onClick={(e) => handleCalculate(e)} aria-hidden="true">
           2
         </li>
-        <li className="cal-item" onClick={() => handleCalculate} aria-hidden="true">
+        <li className="cal-item" onClick={(e) => handleCalculate(e)} aria-hidden="true">
           3
         </li>
 
         <li
           className="cal-item bg-orange "
           id="+"
-          onClick={() => handleCalculate}
+          onClick={(e) => handleCalculate(e)}
           aria-hidden="true"
         >
           <TiPlus />
@@ -120,7 +130,7 @@ const Calculator = () => {
 
         <li
           className="cal-item item-0"
-          onClick={() => handleCalculate}
+          onClick={(e) => handleCalculate(e)}
           aria-hidden="true"
         >
           0
@@ -128,7 +138,7 @@ const Calculator = () => {
         <li
           className="cal-item"
           id="-"
-          onClick={() => handleCalculate}
+          onClick={(e) => handleCalculate(e)}
           aria-hidden="true"
         >
           .
@@ -136,7 +146,7 @@ const Calculator = () => {
         <li
           className="cal-item bg-orange"
           id="="
-          onClick={() => handleCalculate}
+          onClick={(e) => handleCalculate(e)}
           aria-hidden="true"
         >
           <TiEquals />
