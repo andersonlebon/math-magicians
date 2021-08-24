bd32416e4b10e858b44c610a70e6486f43f3b2c3
Caleb Anderson <buyananderson@gmail.com>
2021-08-24 20:32:21 +0200

    Install and set up routers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/home';
+import Navbar from './components/navbar';
+import Quote from './components/quotes';
 
-import './Sass/app.css';
+import './Sass/app.scss';
 
 class App extends Component {
   constructor(props) {
@@ -10,7 +14,16 @@ class App extends Component {
   }
 
   render() {
-    return <Calculator />;
+    return (
+      <>
+        <Route path="/" component={Navbar} />
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/calculater" component={Calculator} />
+          <Route path="/quote" component={Quote} />
+        </Switch>
+      </>
+    );
   }
 }
 
