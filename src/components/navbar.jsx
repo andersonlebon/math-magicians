e43cea13519d1fa41eda37d2f602165b0d7aa743
Caleb Anderson <buyananderson@gmail.com>
2021-08-24 20:42:44 +0200

    Styele tha navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 
 const Navbar = () => (
-  <div>
-    <h1>Navber</h1>
-  </div>
+  <header>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/home">
+        Math magicians
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <a className="nav-item nav-link active" href="/">
+            Home
+            <span className="sr-only">(current)</span>
+          </a>
+          <a className="nav-item nav-link" href="/calculator">
+            Calculator
+          </a>
+          <a className="nav-item nav-link" href="/quotes">
+            Quotes
+          </a>
+        </div>
+      </div>
+    </nav>
+  </header>
 );
 export default Navbar;
