c47aa2ebab3591ae4e73180606b6e084b90aa77e
Caleb Anderson <buyananderson@gmail.com>
2021-08-26 08:43:02 +0200

    Test the operation Function

--- src/test/maths.test.js
+++ src/test/maths.test.js
@@ -1,6 +1,24 @@
 import operate from '../mathLogic/operate';
-import Big from 'big.js';
+// import Big from 'big.js';
 
 describe('Math logic', () => {
-  it('Should test Math oparatios', () => {});
+  it('Should test the addition oparation', () => {
+    const result = operate(1, 2, '+');
+    expect(parseInt(result, 10)).toEqual(3);
+  });
+
+  it('Should test the Substract oparation', () => {
+    const result = operate(10, 2, '-');
+    expect(parseInt(result, 10)).toEqual(8);
+  });
+
+  it('Should test the Multiply oparation', () => {
+    const result = operate(3, 2, 'x');
+    expect(parseInt(result, 10)).toEqual(6);
+  });
+
+  it('Should test the Divide oparation', () => {
+    const result = operate(10, 2, '÷');
+    expect(parseInt(result, 10)).toEqual(5);
+  });
 });
