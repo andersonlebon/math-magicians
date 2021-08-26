b778614ec45083cbe6749da9efb194ecc9402c11
Caleb Anderson <buyananderson@gmail.com>
2021-08-26 08:59:34 +0200

    Test the calculate function when the key AC is pressed

--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -1,5 +1,9 @@
 import calculate from '../mathLogic/calculate';
 
 describe('Check Math calculations', () => {
-  it('Enter first number', () => {});
+  it('When Enter AC key', () => {
+    const obj = { total: null, next: null, operation: null };
+    const restult = calculate(obj, 'AC');
+    expect(restult).toStrictEqual(obj);
+  });
 });
